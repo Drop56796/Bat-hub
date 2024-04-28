3ed10489812ce196bc5edbd97d3d4a59321ecc10
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-04-28 19:40:02 +0800

    Update and rename Bathub V1 (Make In github).lua to Bathub (Make In github).lua

--- Bathub (Make In github).lua
+++ Bathub (Make In github).lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Bathub V1 is ready!",
-   LoadingTitle = "Bathub V1 loading(Main Ui:Arrayfield)",
+   Name = "bathub",
+   LoadingTitle = "Bathub loading(Main Ui:Arrayfield)",
    LoadingSubtitle = "By slience",
    ConfigurationSaving = {
       Enabled = true,
@@ -15,13 +15,13 @@ local Window = Rayfield:CreateWindow({
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Create Checkpoint 1",
+      Title = "Checkpoint 1",
       Subtitle = "Key System",
       Note = "Bilibili:slienceScript",
       FileName = "SiriusKey",
       SaveKey = false,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = "Bathub Key system bypass"
+      Key = "BatKey_45078b972538a846"
    }
 })
 local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
@@ -100,9 +100,9 @@ local Button = Tab:CreateButton({
    end,
 })
 
-local Label = Tab:CreateLabel("BatHub V1 is loading Welcome",Section)
+local Label = Tab:CreateLabel("BatHub is loading Welcome",Section)
 local Paragraph = Tab:CreateParagraph({Title = "Update", Content = "Part of the script mode loading"},Section)
-local Sets = Tab:CreateSection('Set Functions',false)
+local Paragraph = Tab:CreateParagraph({Title = "Version", Content = "V1.1"},Section)
 local Section = Tab:CreateSection("Hub",false) -- The 2nd argument is to tell if its only a Title and doesnt contain element
 local Button = Tab:CreateButton({
    Name = "MSHUB V5",
@@ -110,5 +110,23 @@ local Button = Tab:CreateButton({
    Interact = '●Button●',
    Callback = function()
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/MSDOORS/main/MSHUB_Loader.lua"),true))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Catheter Hub",
+   Info = "Button info/Description.", -- Speaks for itself, Remove if none.
+   Interact = '●Button●',
+   Callback = function()
+   loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\117\115\101\114\97\110\101\119\114\102\102\47\114\111\98\108\111\120\45\47\109\97\105\110\47\37\69\54\37\57\68\37\65\49\37\69\54\37\65\67\37\66\69\37\69\53\37\56\68\37\56\70\37\69\56\37\65\69\37\65\69\34\41\41\40\41\10")()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "slience Hub",
+   Info = "Button info/Description.", -- Speaks for itself, Remove if none.
+   Interact = '●Button●',
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/sdhdbxub/sdhdbxub/main/sjznwijxwislf.TXT"))()
    end,
 })
